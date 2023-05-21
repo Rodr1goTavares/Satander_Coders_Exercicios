f72f1b66c49e0d0f26b2ebbd5bb7ea4171398429
Rodrigo Tavares <86687715+Rodr1goTavares@users.noreply.github.com>
2023-05-21 17:03:36 -0300

    imports corrigidos

--- coders_exercices/Main.java
+++ coders_exercices/Main.java
@@ -1,17 +1,21 @@
 package coders_exercices;
 
-import coders_exercices.ex_1.Tabuada;
 import coders_exercices.ex_6_final_challange.TipCalculator;
+import coders_exercices.ex_1.Tabuada;
+import coders_exercices.ex_2.CelsiusConversor;
+import coders_exercices.ex_3.FruitList;
+import coders_exercices.ex_4.Promotion;
+import coders_exercices.ex_5.Promotion2;
 
 public class Main {
     public static void main(String[] args) {
         // <<< Todos os exercícios abaixo: >>> 
         
-        Tabuada.start();
+        // Tabuada.start();
         // CelsiusConversor.start();
         // FruitList.start();
         // Promotion.start();
         // Promotion2.start();
-        // TipCalculator.start();
+        TipCalculator.start();
     }
 }
